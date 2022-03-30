f82b1b1000b1b1ab7f627c2fbbf4916ba8c5b6ff
user3 <user3@user3.ru>
2022-03-30 14:21:11 +0500

    Добавьте файлы проекта.

---
+++ PizzeriaApp/View/Pages/AuthPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PizzeriaApp.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+            //this.NavigationService.Navigate(new MainPage());
+        }
+
+        
+        private void AuthorisationClick(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new WelcomePage());
+            try
+            {
+                //считаем количество записей в таблице с заданными параметрами (логин, пароль)
+                int имяПеременной = полключениеБД.context.имяТаблицы.Where(
+                x => x.Login == имяПоля.Text && x.Password == имяПоля.Password
+                ).Count();
+
+                if (имяПеременной == 0)
+                {
+                    MessageBox.Show("Такой пользователь отсутствует!",
+                    "Уведомление",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+
+                }
+                else
+                {
+                    switch (имяПеременной.IdRole)
+                    {
+                        case 1:
+                            this.NavigationService.Navigate(new страница1());
+
+                            break;
+                        case 2:
+                            this.NavigationService.Navigate(new страница2());
+
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Критический сбор в работе приложения:" + ex.Message.ToString(),
+                "Уведомление",
+                MessageBoxButton.OK,
+                MessageBoxImage.Warning);
+            }
+        }
+    }
+}
---
+++ PizzeriaApp/View/Pages/WelcomePage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PizzeriaApp.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для WelcomePage.xaml
+    /// </summary>
+    public partial class WelcomePage : Page
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void ClientButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new ClientPage());
+        }
+        private void ProductButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new ProductPage());
+        }
+
+        
+    }
+}
